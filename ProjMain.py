3068e3f2d47614c6bc442862b1df7805c7bc0538
김준현/트렌드플랫폼팀 <junhyeon.kim@gsretail.com>
2022-12-23 17:25:39 +0900

    projmain.py code modi

--- ProjMain.py
+++ ProjMain.py
@@ -2,6 +2,7 @@ try:
     
     import sys 
     import re 
+    import time
     import requests
     from elasticsearch import Elasticsearch
     from elasticsearch.helpers import bulk
@@ -104,6 +105,53 @@ def get_media_source(news_source: bs4.element.Tag)\
         # empty string return
         return ""
 
+def get_newsct_tag(bs_obj: bs4.element.Tag)\
+        -> tuple:
+    '''
+    :param bs_obj:
+    :return:
+    '''
+    try:
+        
+        newsct :bs4.element.Tag= bs_obj.select_one("div#ct.newsct") 
+        if newsct != None:
+            return newsct, True
+        else:
+            return newsct, False
+    except:
+        return None, False
+    
+def get_news_header_tag(newsct: bs4.element.Tag)\
+        -> tuple:
+    '''
+    :param newsct:
+    :return:
+    '''
+    try:
+        news_header :bs4.element.Tag= newsct.select_one("div.media_end_head.go_trans")
+        if news_header != None:
+            return news_header, True
+        else:
+            return news_header, False
+    except:
+        return None, False
+
+def get_news_body_tag(newsct: bs4.element.Tag)\
+        -> tuple:
+    '''
+    :param newsct:
+    :return:
+    '''
+    try:
+        
+        news_body :bs4.element.Tag= newsct.select_one("div#contents.newsct_body") 
+        if news_body != None:
+            return news_body, True
+        else:
+            return news_body, False
+    except:
+        return None, False
+    
 def get_news_detail_information(u: dict, url_header: dict[str, str])\
     -> dict:
     '''
@@ -113,46 +161,47 @@ def get_news_detail_information(u: dict, url_header: dict[str, str])\
     '''
     element_template = Template()
     
-    #element :dict = {"req_url": u["url"]}
     element_template.news_url += u["url"]
     
     response = requests.get(u["url"], headers= url_header) 
     
     if response.status_code == 200:
         bs_obj = BeautifulSoup(response.text, "html.parser")
-    
-        newsct :bs4.element.Tag= bs_obj.select_one("div#ct.newsct")
-        news_header :bs4.element.Tag= newsct.select_one("div.media_end_head.go_trans")
-        news_source :bs4.element.Tag= news_header.select_one("div.media_end_head_top")
-        news_title :bs4.element.Tag= news_header.select_one("div.media_end_head_title")
-        news_body :bs4.element.Tag= newsct.select_one("div#contents.newsct_body")
-    
-        news_source_v :str= get_media_source(news_source= news_source) # 뉴스 출처
-        news_title_v :str= get_media_title(news_title= news_title) # 뉴스 타이틀
-        news_body_v :str= get_media_body(news_body=news_body)
 
-        element_template.news_source += news_source_v
-        element_template.news_title += news_title_v
-        element_template.news_body += news_body_v
-        element_template.news_sid1_num += u["sid1"]
-        element_template.news_sid1_hangl_cate += u["sid1_hangl_cate"]
-        element_template.news_sid2_num += u["sid2"]
-        element_template.news_sid2_hangl_cate += u["sid2_hangl_cate"]
-        element_template.news_cllct_detail_time += u["detail_cllct_time"] 
+        newsct, is_check_newsct = get_newsct_tag(bs_obj=bs_obj)
+        if is_check_newsct:
+            news_header, is_check_news_header = get_news_header_tag(newsct= newsct)
+            news_body, is_check_news_body = get_news_body_tag(newsct= newsct)
+            
+            if is_check_news_header:
+                news_source :bs4.element.Tag= news_header.select_one("div.media_end_head_top")
+                news_source_v :str= get_media_source(news_source= news_source) # 뉴스 출처
+                element_template.news_source += news_source_v
+                
+                news_title :bs4.element.Tag= news_header.select_one("div.media_end_head_title")
+                news_title_v :str= get_media_title(news_title= news_title) # 뉴스 타이틀
+                element_template.news_title += news_title_v
+            else:
+                ''' is_check_news_header == False 
+                '''
+                
+            if is_check_news_body:
+                news_body_v :str= get_media_body(news_body=news_body)
+                element_template.news_body += news_body_v
+            else:
+                ''' is_check_news_body == False 
+                '''
+        else:
+            ''' is_check_newsct == False
+            '''
         
-        '''
-        element["news-source"] = news_source_v    
-        element["news-title"] = news_title_v
-        element["news-body"] = news_body_v
-        element["news-sid1_num"] = u["sid1"]
-        element["news-sid1_hangl_cate"] = u["sid1_hangl_cate"]
-        element["news-sid2_num"] = u["sid2"]
-        element["news-sid2_hangl_cate"] = u["sid2_hangl_cate"]
-        element["news-cllct_detail_time"] = u["detail_cllct_time"]
-        '''
-        return asdict(element_template)
+    element_template.news_sid1_num += u["sid1"]
+    element_template.news_sid1_hangl_cate += u["sid1_hangl_cate"]
+    element_template.news_sid2_num += u["sid2"]
+    element_template.news_sid2_hangl_cate += u["sid2_hangl_cate"]
+    element_template.news_cllct_detail_time += u["detail_cllct_time"] 
     
-    
+    return asdict(element_template)
 
 ## -----------------------------
 # PROJ MAIN FUNCTION
@@ -176,7 +225,7 @@ if __name__ == "__main__":
     
     sid2 :dict[str, int] = o._obj._sid2
     for k, v in sid2.items():
-        for page in range(1, 2):
+        for page in range(1, 11):
             url:list[dict] = o.get_url_list(
                 sid2=v, 
                 page=page, 
